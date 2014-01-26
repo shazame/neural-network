7e51298dbee8c74e883b901b540dcaf8769e6297
Thibaud Lambert <thibaud.lambert@gmail.com>
2014-01-26 22:45:04 +0100

    Change to respect Dale law

--- src/neuralNetwork.py
+++ src/neuralNetwork.py
@@ -5,81 +5,81 @@ import action
 import time
 
 class NeuralNetwork:
-    def __init__(self):
-        self.neurones = []
-        self.stimulus = []
+  def __init__(self):
+    self.neurones = []
+    self.stimulus = []
 
-    def addStimulus(self, stimulus):
-        """ Add the stimulus given in parameter to the neural network"""
-        self.stimulus += [stimulus]
+  def addStimulus(self, stimulus):
+    """ Add the stimulus given in parameter to the neural network"""
+    self.stimulus += [stimulus]
 
-    def addNeuron(self, neuron):
-        """ Add the neuron given in parameter to the neural network"""
-        self.neurones += [neuron]
+  def addNeuron(self, neuron):
+    """ Add the neuron given in parameter to the neural network"""
+    self.neurones += [neuron]
 
-    def tick(self,display=False):
-        """ Execute one tick of the neural network"""
-        for n in self.neurones:
-            n.activationFunction()
-        for n in self.neurones:
-            n.updateState()
-        if display:
-            self.display()
+  def tick(self,display=False):
+    """ Execute one tick of the neural network"""
+    for n in self.neurones:
+      n.activationFunction()
+    for n in self.neurones:
+      n.updateState()
+    if display:
+      self.display()
 
-    def display(self):
-        """ Display the neural network"""
-        print()
-        for n in self.neurones:
-            print(n)
+  def display(self):
+    """ Display the neural network"""
+    print()
+    for n in self.neurones:
+      print(n)
 
-    def run(self, nbTick, display=False):
-        """ Execute nbTick if the neural network"""
-        for i in nbTick:
-            self.tick(display)
+  def run(self, nbTick, display=False):
+    """ Execute nbTick if the neural network"""
+    for i in nbTick:
+      self.tick(display)
 
 
 if __name__ == "__main__":
-    #Stimulus
-    s1 = stimulus.Stimulus("s1")
-    s2 = stimulus.Stimulus("s2")
+  #Stimulus
+  s1 = stimulus.Stimulus("s1")
+  s2 = stimulus.Stimulus("s2")
 
-    #Neurones
-    n1 = neuron.ThresholdNeuron("n1",2)
-    n2 = neuron.ThresholdNeuron("n2",2)
-    n3 = neuron.ThresholdNeuron("n3",1)
-    n4 = neuron.ThresholdNeuron("n4",1)
+  #Neurones
+  n1 = neuron.ThresholdNeuron("n1",2)
+  n2 = neuron.ThresholdNeuron("n2",2,True)
+  n3 = neuron.ThresholdNeuron("n3",1)
+  n4 = neuron.ThresholdNeuron("n4",1)
 
-    #Actions
-    a1 = action.Action("a1")
-    a2 = action.Action("a2")
+  #Actions
+  a1 = action.Action("a1")
+  a2 = action.Action("a2")
 
-    #Connections
-    n1.connect(n3)
-    n3.connect(n2,-1)
-    n2.connect(n4)
-    n4.connect(n1,-1)
+  #Connections
+  n1.connect(n3)
+  n3.connect(n2,1)
+  n2.connect(n4)
+  n4.connect(n1,1)
 
-    s1.addNeuron(n1)
-    s2.addNeuron(n2)
+  s1.addNeuron(n1)
+  s2.addNeuron(n2)
 
-    n1.addAction(a1)
-    n2.addAction(a2)
-     
-    #Neural Network
-    network = NeuralNetwork()
-    network.addNeuron(n1)
-    network.addNeuron(n2)
-    network.addNeuron(n3)
-    network.addNeuron(n4)
-    network.addStimulus(s1)
-    network.addStimulus(s2)
+  n1.addAction(a1)
+  n2.addAction(a2)
+   
+  #Neural Network
+  network = NeuralNetwork()
+  network.addNeuron(n1)
+  network.addNeuron(n2)
+  network.addNeuron(n3)
+  network.addNeuron(n4)
+  network.addStimulus(s1)
+  network.addStimulus(s2)
 
 
-    for i in range(10):
-        if (i % 2) == 0:
-            s1.charge(1.5)
-        if (i % 4) == 1:
-            s2.charge(1.5)
-        network.tick(True)
-        time.sleep(1) 
+  for i in range(10):
+    if (i % 2) == 0:
+      s1.charge(1.5)
+    if (i % 4) == 1:
+      s2.charge(1.5)
+    network.tick(True)
+    time.sleep(1) 
 
--- src/neuron.py
+++ src/neuron.py
@@ -1,105 +1,115 @@
 #!/usr/bin/env python
 
 class Neuron:
-    def __init__(self, name):
-        self.name = name
-        self.inputNeurones = []
-        self.inputFactor = []
-        self.outputNeurones = []
-        self.outputActions = []
-        self.state = 0.0
-        self.nextState = 0.0
+  def __init__(self, name, isInhibitor = False):
+    self.name = name
+    self.inputNeurones = []
+    self.inputFactor = []
+    self.outputNeurones = []
+    self.outputActions = []
+    self.state = 0.0
+    self.nextState = 0.0
+    self.inhibitorFactor = (-1.0 if isInhibitor else 1.0)
 
-    def __str__(self):
-        return "(%s) state: %f\toutput: %s \tinput: %s" % (self.name, self.state,
-        ', '.join(n.name for n in self.outputNeurones),
-        ', '.join(n.name for n in self.inputNeurones))
+  def __str__(self):
+    return "(%s) state: %f\toutput: %s \tinput: %s" % (self.name, self.state,
+    ', '.join(n.name for n in self.outputNeurones),
+    ', '.join(n.name for n in self.inputNeurones))
 
-    def addAction(self, action):
-        self.outputActions += [action]
+  def addAction(self, action):
+    self.outputActions += [action]
 
-    def connect(self, neuron, factor = 0.5):
-        """ Connects the neuron's output to another neuron. """
-        if not neuron in self.outputNeurones:
-            self.outputNeurones += [neuron]
-            neuron.connectFrom(self, factor)
+  def connect(self, neuron, factor = 0.5):
+    """ Connects the neuron's output to another neuron. """
+    if factor > 0 :
+      if not neuron in self.outputNeurones:
+        self.outputNeurones += [neuron]
+        neuron.connectFrom(self, factor)
+    else :
+      print "Factor have to be positive"
 
-    def connectFrom(self, neuron, factor = 0.5):
-        """ Connects the neuron's input to another neuron. """
-        if not neuron in self.inputNeurones:
-            self.inputNeurones += [neuron]
-            self.inputFactor += [factor]
-            neuron.connect(self, factor)
+  def connectFrom(self, neuron, factor = 0.5):
+    """ Connects the neuron's input to another neuron. """
+    if factor > 0 :
+      if not neuron in self.inputNeurones:
+        self.inputNeurones += [neuron]
+        self.inputFactor += [factor]
+        neuron.connect(self, factor)
+    else:
+      print "Factor have to be positive"
 
-    def updateState(self):
-        """ Put the neuron into the next state"""
-        self.state = self.nextState
+  def updateState(self):
+    """ Put the neuron into the next state"""
+    self.state = self.nextState
 
-    def activationFunction(self):
-        """ Function of activation for this neurone """
-            
-    def charge(self, stimulus):
-        """ Charge the neuron """
-        self.nextState += stimulus
+  def activationFunction(self):
+    """ Function of activation for this neurone """
+      
+  def charge(self, stimulus):
+    """ Charge the neuron """
+    self.nextState += stimulus
 
-    def send(self, neuron, stimulus):
-        """ Send a stimuli to the neuron. """
-        index = self.inputNeurones.index(neuron)
-        self.nextState += stimulus * self.inputFactor[index]
-        if self.nextState < 0 :
-            self.nextState = 0
+  def send(self, neuron, stimulus):
+    """ Send a stimuli to the neuron. """
+    index = self.inputNeurones.index(neuron)
+    self.nextState += stimulus * self.inputFactor[index]
+    if self.nextState < 0 :
+      self.nextState = 0
 
-    def discharge(self):
-        """ Discharge the neuron, sending stimuli on its outputs. """
-        for a in self.outputActions:
-            a.activate()
-        for o in self.outputNeurones:
-            o.send(self, self.state)
-        self.nextState -= self.state
-        if self.nextState < 0 :
-            self.nextState = 0
+  def discharge(self):
+    """ Discharge the neuron, sending stimuli on its outputs. """
+    for a in self.outputActions:
+      a.activate()
+    for o in self.outputNeurones:
+      o.send(self, self.state * self.inhibitorFactor)
+    self.nextState -= self.state
+    if self.nextState < 0 :
+      self.nextState = 0
 
 class ThresholdNeuron(Neuron):
-    def __init__(self, name, threshold):
-        Neuron.__init__(self, name)
-        self.threshold = threshold
+  def __init__(self, name, threshold, isInhibitor = False):
+    Neuron.__init__(self, name, isInhibitor)
+    self.threshold = threshold
 
-    def activationFunction(self):
-        """ Function of activation for this neurone """
-        if self.state > self.threshold :
-            self.discharge()
+  def activationFunction(self):
+    """ Function of activation for this neurone """
+    if self.state > self.threshold :
+      self.discharge()
 
 
 if __name__ == "__main__":
-    n1 = Neuron("n1")
-    n2 = ThresholdNeuron("n2",1)
-    n3 = ThresholdNeuron("n3",2)
-    n4 = Neuron("n4")
-    n1.connect(n2)
-    n1.connect(n3)
-    n2.connect(n3,-1)
-    n2.connect(n4)
-    n3.connect(n4)
+  n1 = Neuron("n1")
+  n2 = ThresholdNeuron("n2",1,True)
+  n3 = ThresholdNeuron("n3",2)
+  n1.connect(n2)
+  n1.connect(n3,2)
+  n2.connect(n3,1)
 
-    print("Set the initial state of n1 to 2:")
-    n1.charge(2.3)
-    print(n1)
-    print(n2)
-    print(n3)
-    print(n4)
+  print("Set the initial state of n1 to 2:")
+  n1.charge(2.3)
+  n1.updateState()
+  n2.updateState()
+  n3.updateState()
+  print(n1)
+  print(n2)
+  print(n3)
 
-    print("")
-    print("Discharge n1:")
-    n1.discharge()
-    print(n1)
-    print(n2)
-    print(n3)
-    print(n4)
+  print("")
+  print("Discharge n1:")
+  n1.discharge()
+  n1.updateState()
+  n2.updateState()
+  n3.updateState()
+  print(n1)
+  print(n2)
+  print(n3)
 
-    print("")
-    print("Discharge n2:")
-    n2.discharge()
-    print(n1)
-    print(n2)
-    print(n3)
-    print(n4)
+  print("")
+  print("Discharge n2:")
+  n2.discharge()
+  n1.updateState()
+  n2.updateState()
+  n3.updateState()  
+  print(n1)
+  print(n2)
+  print(n3)
